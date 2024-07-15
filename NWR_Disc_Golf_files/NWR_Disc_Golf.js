0dcb5fb5cf1e2c949165a1b06e170fc3815819ea
blairstown <blairstown@blairstown.com>
2024-07-14 21:13:37 -0400

    more files.

---
+++ NWR_Disc_Golf_files/NWR_Disc_Golf.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.3 local-build-20131213
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('NWR_Disc_Golf_files/NWR_Disc_GolfMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Nardone_Electric_files/Nardone_Electric.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20131213
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Nardone_Electric_files/Nardone_ElectricMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');applyEffects()}
