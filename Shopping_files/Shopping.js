7269113e919fdc81f725e5ea7898942e9e920a3c
blairstown <blairstown@blairstown.com>
2024-07-14 21:15:28 -0400

    more files.

---
+++ Shopping_files/Shopping.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20131213
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Shopping_files/ShoppingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
