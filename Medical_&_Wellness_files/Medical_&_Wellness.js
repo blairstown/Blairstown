8db52f57f610069a0bf3db841ad1c9504352d6e2
blairstown <blairstown@blairstown.com>
2024-07-14 21:15:07 -0400

    more files.

---
+++ Medical_&_Wellness_files/Medical_&_Wellness.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20131213
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Medical_&_Wellness_files/Medical_&_WellnessMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
